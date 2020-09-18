8c17a739dcbd99c3f43f9011f2d05789b0248e17
matteuscavl <mateuslcavl@gmail.com>
2020-09-18 16:32:25 -0300

    Finalizado o projeto my website

--- js/script.js
+++ js/script.js
@@ -65,3 +65,14 @@ if(targetNime.length) {
     })
 }
 
+/* SCROLL SUAVE */
+
+$('.main-header a').click(function(e) {
+    e.preventDefault();
+
+    let id = $(this).attr('href')
+    targetOffset = $(id).offset().top;
+    $('html, body').animate({
+        scrollTop: targetOffset - 80
+    }, 500)
+})
