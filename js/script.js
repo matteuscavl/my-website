a5b120f0975c34f04adbe3748cc0bdeefe51508a
matteuscavl <mateuslcavl@gmail.com>
2020-09-30 22:37:20 -0300

    Aplicado efeitos no Container

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 const btnMenu = $('.checkbtn');
 const menu = $('.navigation-menu-list')
 const title = document.querySelector('.h1header');
+const consoleh2 = document.querySelectorAll('.h2text');
+const paragraphp2 = document.querySelectorAll('.pclass')
+const targetAnime = document.querySelectorAll('[data-anime]')
 
 btnMenu.click(function() {
     if(menu.hasClass('navigation-menu-list-toggle')) {
@@ -18,8 +21,34 @@ elemento.innerHTML = '';
     textArray.forEach((letra, i) => {
         setTimeout(function () {
             elemento.innerHTML += letra
-        }, 80 * i)
+        }, 50 * i)
     })
 }
 
 maquinaEscreverEffect(title);
+
+consoleh2.forEach((item) => {
+    maquinaEscreverEffect(item);
+})
+
+paragraphp2.forEach((item) => {
+    maquinaEscreverEffect(item);
+})
+
+
+const efeitoContainer = () => {
+    const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4);
+    targetAnime.forEach(function(elemento) {
+        if(windowTop > elemento.offsetTop) {
+            elemento.classList.add('animateTop');
+        } else {
+            elemento.classList.remove('animateTop')
+        }
+    })
+}
+
+if (targetAnime.length) {
+    window.addEventListener('scroll', function() {
+        efeitoContainer();
+    })
+}
